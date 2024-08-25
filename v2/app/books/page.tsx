f106251eadf09ba6d2ec22746af69c137bc90e62
alan-w25 <alan.wusig@gmail.com>
2024-08-25 17:06:27 -0400

    change the order

--- v2/app/books/page.tsx
+++ v2/app/books/page.tsx
@@ -18,6 +18,28 @@ export interface Book {
   title: string;
 }
 
+export default function BookPage() {
+  return (
+    <main>
+      <Header currentUnderline='books' />
+      <div id="code-project-description-header" className="flex w-5/6 flex-col mx-auto mt-20">
+        <h1 className="text-3xl font-bold text-black">Books</h1>
+        <p className="mt-8 text-md text-black">
+          I am always looking for a new book to read. I love reading anything; from fiction to non-fiction, I love to explore new worlds and ideas. Currently, 
+          I have been into books that explore the human experience and books that help me grow as a person. Below are some of the books that I&apos;ve read and thoughts.
+
+          <br/>
+          <br/>
+
+          <p>Coming soon!</p>
+        </p>
+
+      </div>
+      <Footer />
+    </main>
+  )
+}
+
 export const booksList: Book[] = [
   {
     imageSrc: norwegianWood, 
@@ -51,24 +73,4 @@ export const booksList: Book[] = [
   }
 ];
 
-export default function books() {
-  return (
-    <main>
-      <Header currentUnderline='books' />
-      <div id="code-project-description-header" className="flex w-5/6 flex-col mx-auto mt-20">
-        <h1 className="text-3xl font-bold text-black">Books</h1>
-        <p className="mt-8 text-md text-black">
-          I am always looking for a new book to read. I love reading anything; from fiction to non-fiction, I love to explore new worlds and ideas. Currently, 
-          I have been into books that explore the human experience and books that help me grow as a person. Below are some of the books that I&apos;ve read and thoughts.
 
-          <br/>
-          <br/>
-
-          <p>Coming soon!</p>
-        </p>
-
-      </div>
-      <Footer />
-    </main>
-  )
-}
